e5c762b0fb870e505d574f60b2792bc8c0339643
A. Lahatrarivelo ANDRIAMAMONJY <lahatrarivelo.andriamamonjy@gmail.com>
2025-08-14 14:52:04 +0300

    Restructuring backend folders, tokens:not finished

--- back/core-service/app.ts
+++ back/core-service/app.ts
@@ -4,10 +4,10 @@ import swaggerUi from "swagger-ui-express";
 import swaggerOptions from "./swagger";
 
 import router from "./routes";
-// import Hasher from "./utils/Hasher.js"; // this may become .ts later
 
 const app = express();
 app.use(express.json());
+app.use(router);
 
 const specs = swaggerJsdoc(swaggerOptions);
 
@@ -19,14 +19,6 @@ app.listen(port, () => {
   console.log("Starting server...");
   console.log(`Server listening on port ${port}`);
   console.log(`Swagger UI docs at http://localhost:${port}/api-docs`);
-});
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, Express!");
-});
-
-app.get("/test", (req: Request, res: Response) => {
-  res.send("Hello, Test Express!");
 });
 
 /**
--- back/core-service/routes/index.ts
+++ back/core-service/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 
-import authRouter from "./auth/routes";
+import authRouter from "./AuthRoutes";
+import tokenRouter from "./TokenRoutes";
 
 const router = Router();
 router.use(authRouter);
+router.use(tokenRouter);
 
 export default router
