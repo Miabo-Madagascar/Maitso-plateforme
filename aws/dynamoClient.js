c1792fa8ecb7cb7f94a2d2d2e39f291d7bbb91cd
masytana <145836846+masytana@users.noreply.github.com>
2025-09-24 22:46:48 +0300

    Create dynamoClient.js

---
+++ aws/dynamoClient.js
@@ -0,0 +1,28 @@
+const AWS = require('aws-sdk');
+
+const REGION = process.env.AWS_REGION_MAITSO;
+const TABLE_NAME = process.env.DYNAMODB_TABLE_MAITSO;
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient({ region: REGION });
+
+async function getAllItems() {
+  try {
+    const data = await dynamoDB.scan({ TableName: TABLE_NAME }).promise();
+    return data.Items;
+  } catch (err) {
+    console.error('Erreur DynamoDB:', err);
+    return [];
+  }
+}
+
+async function putItem(item) {
+  try {
+    await dynamoDB.put({ TableName: TABLE_NAME, Item: item }).promise();
+    return true;
+  } catch (err) {
+    console.error('Erreur DynamoDB:', err);
+    return false;
+  }
+}
+
+module.exports = { getAllItems, putItem };
